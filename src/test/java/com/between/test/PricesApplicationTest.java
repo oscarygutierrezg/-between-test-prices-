0067edc539bc4fb6287b93f91d38c8d70f1f9d26
Oscar Gutiérrez <backend7m5@apexglobal.co>
2024-11-16 07:26:51 -0300

    Project structure configuration

---
+++ src/test/java/com/between/test/PricesApplicationTest.java
@@ -0,0 +1,18 @@
+package com.between.test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+public class PricesApplicationTest {
+
+	@BeforeAll
+	public void init() {}
+
+	@Test
+	public void contextLoads() {
+	}
+}
